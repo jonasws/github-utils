8f51da4ba648e8af15403a6ac71bf2fd9134a66d
Jonas Strømsodd <jonas.stromsodd@gmail.com>
2016-02-22 12:03:42 +0100

    Use const modifier extensively
    it is good practice, and an extra layer of guarantee for accidental
    overwriting to use `const` over `let` whenever possible.

--- spec/github-utils-spec.js
+++ spec/github-utils-spec.js
@@ -6,39 +6,39 @@ describe('module github-utils', () => {
   describe('function parseRepositoryInfoFromURL', () => {
 
     it('should parse the owner and name of a ssh-style repository url', () => {
-      let exampleRepoURL = 'git@github.com:someuser/somesweetrepo';
-      let [repoOwner, repoName] = parseRepositoryInfoFromURL(exampleRepoURL);
+      const exampleRepoURL = 'git@github.com:someuser/somesweetrepo';
+      const [repoOwner, repoName] = parseRepositoryInfoFromURL(exampleRepoURL);
 
       expect(repoOwner).toEqual('someuser');
       expect(repoName).toEqual('somesweetrepo');
     });
 
     it('should parse the owner and name of a http-style repository url', () => {
-      let exampleRepoURL = 'https://github.com/someuser/somesweetrepo.git';
-      let [repoOwner, repoName] = parseRepositoryInfoFromURL(exampleRepoURL);
+      const exampleRepoURL = 'https://github.com/someuser/somesweetrepo.git';
+      const [repoOwner, repoName] = parseRepositoryInfoFromURL(exampleRepoURL);
 
       expect(repoOwner).toEqual('someuser');
       expect(repoName).toEqual('somesweetrepo');
     });
 
     it('should parse the owner and name of a subersion-style repository url', () => {
-      let exampleRepoURL = 'https://github.com/someuser/somesweetrepo';
-      let [repoOwner, repoName] = parseRepositoryInfoFromURL(exampleRepoURL);
+      const exampleRepoURL = 'https://github.com/someuser/somesweetrepo';
+      const [repoOwner, repoName] = parseRepositoryInfoFromURL(exampleRepoURL);
 
       expect(repoOwner).toEqual('someuser');
       expect(repoName).toEqual('somesweetrepo');
     });
 
     it('should return null if no information can be parsed', () =>{
-      let invalidRepoURL = 'git@notighub.com/user/repo';
-      let info = parseRepositoryInfoFromURL(invalidRepoURL);
+      const invalidRepoURL = 'git@notighub.com/user/repo';
+      const info = parseRepositoryInfoFromURL(invalidRepoURL);
       expect(info).toBeNull();
     });
   });
 
   describe('function getPullRequestURLS', () => {
     it('should return the HTML urls of the pull requests', () => {
-      let examplePullRequests = [
+      const examplePullRequests = [
         {
           html_url: 'https://someurl',
           head: {
@@ -58,7 +58,7 @@ describe('module github-utils', () => {
           }
         }
       ];
-      let expectedURLs = ['https://someurl', 'https://someotherurl'];
+      const expectedURLs = ['https://someurl', 'https://someotherurl'];
 
       expect(getPullRequestURLs(examplePullRequests, 'master')).toEqual(expectedURLs);
     });
